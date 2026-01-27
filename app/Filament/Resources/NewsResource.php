91c08cc08f009e09f5332cb51aa72b6a7cb814f2
Beni80amz <benisolehudin@madrasah.kemenag.go.id>
2026-01-28 01:59:56 +0700

    feat: apply tiptap editor to news resource

--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -28,123 +28,150 @@ class NewsResource extends Resource
     {
         return $form
             ->schema([
-                    Forms\Components\Grid::make(3)
-                        ->schema([
-                                Forms\Components\Section::make()
-                                    ->columnSpan(2)
-                                    ->schema([
-                                            Forms\Components\TextInput::make('title')
-                                                ->required()
-                                                ->maxLength(255)
-                                                ->live(onBlur: true)
-                                                ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
-                                            Forms\Components\TextInput::make('slug')
-                                                ->required()
-                                                ->maxLength(255)
-                                                ->unique(ignoreRecord: true),
-                                            Forms\Components\RichEditor::make('content')
-                                                ->required()
-                                                ->columnSpanFull(),
-                                            Forms\Components\Textarea::make('excerpt')
-                                                ->rows(3)
-                                                ->columnSpanFull(),
-                                        ]),
-                                Forms\Components\Section::make()
-                                    ->columnSpan(1)
-                                    ->schema([
-                                            Forms\Components\FileUpload::make('featured_image')
-                                                ->image()
-                                                ->directory('news')
-                                                ->required(),
-                                            Forms\Components\Select::make('category_id')
-                                                ->relationship('category', 'name', fn(Builder $query) => $query->where('type', 'news'))
-                                                ->searchable()
-                                                ->preload()
-                                                ->createOptionForm([
-                                                        Forms\Components\TextInput::make('name')
-                                                            ->required()
-                                                            ->maxLength(255)
-                                                            ->live(onBlur: true)
-                                                            ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
-                                                        Forms\Components\TextInput::make('slug')
-                                                            ->required()
-                                                            ->maxLength(255),
-                                                        Forms\Components\Hidden::make('type')->default('news'),
-                                                    ])
-                                                ->required(),
-                                            Forms\Components\Select::make('user_id')
-                                                ->relationship('author', 'name')
-                                                ->default(fn() => auth()->id())
-                                                ->searchable()
-                                                ->required()
-                                                ->label('Penulis'),
-                                            Forms\Components\Select::make('status')
-                                                ->options([
-                                                        'draft' => 'Draft',
-                                                        'published' => 'Published',
-                                                        'archived' => 'Archived',
-                                                    ])
-                                                ->default('draft')
-                                                ->required(),
-                                            Forms\Components\DateTimePicker::make('published_at')
-                                                ->default(now()),
-                                        ]),
+                Forms\Components\Grid::make(3)
+                    ->schema([
+                        Forms\Components\Section::make()
+                            ->columnSpan(2)
+                            ->schema([
+                                Forms\Components\TextInput::make('title')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                                Forms\Components\TextInput::make('slug')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->unique(ignoreRecord: true),
+                                \Awcodes\FilamentTiptapEditor\TiptapEditor::make('content')
+                                    ->required()
+                                    ->columnSpanFull()
+                                    ->tools([
+                                        'heading',
+                                        'bold',
+                                        'italic',
+                                        'strike',
+                                        'underline',
+                                        'lead',
+                                        'bullet-list',
+                                        'ordered-list',
+                                        'checked-list',
+                                        'blockquote',
+                                        'hr',
+                                        'table',
+                                        'link',
+                                        'media',
+                                        'align-left',
+                                        'align-center',
+                                        'align-right',
+                                        'align-justify',
+                                        'direction',
+                                        'code-block',
+                                        'details',
+                                        'remove-format',
+                                        'undo',
+                                        'redo',
+                                    ])
+                                    ->extraInputAttributes(['dir' => 'auto', 'style' => 'min-height: 300px']),
+                                Forms\Components\Textarea::make('excerpt')
+                                    ->rows(3)
+                                    ->columnSpanFull(),
                             ]),
-                ]);
+                        Forms\Components\Section::make()
+                            ->columnSpan(1)
+                            ->schema([
+                                Forms\Components\FileUpload::make('featured_image')
+                                    ->image()
+                                    ->directory('news')
+                                    ->required(),
+                                Forms\Components\Select::make('category_id')
+                                    ->relationship('category', 'name', fn(Builder $query) => $query->where('type', 'news'))
+                                    ->searchable()
+                                    ->preload()
+                                    ->createOptionForm([
+                                        Forms\Components\TextInput::make('name')
+                                            ->required()
+                                            ->maxLength(255)
+                                            ->live(onBlur: true)
+                                            ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                                        Forms\Components\TextInput::make('slug')
+                                            ->required()
+                                            ->maxLength(255),
+                                        Forms\Components\Hidden::make('type')->default('news'),
+                                    ])
+                                    ->required(),
+                                Forms\Components\Select::make('user_id')
+                                    ->relationship('author', 'name')
+                                    ->default(fn() => auth()->id())
+                                    ->searchable()
+                                    ->required()
+                                    ->label('Penulis'),
+                                Forms\Components\Select::make('status')
+                                    ->options([
+                                        'draft' => 'Draft',
+                                        'published' => 'Published',
+                                        'archived' => 'Archived',
+                                    ])
+                                    ->default('draft')
+                                    ->required(),
+                                Forms\Components\DateTimePicker::make('published_at')
+                                    ->default(now()),
+                            ]),
+                    ]),
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                    Tables\Columns\ImageColumn::make('featured_image'),
-                    Tables\Columns\TextColumn::make('title')
-                        ->searchable()
-                        ->limit(50),
-                    Tables\Columns\TextColumn::make('category.name')
-                        ->badge()
-                        ->color('info')
-                        ->searchable(),
-                    Tables\Columns\TextColumn::make('author.name')
-                        ->toggleable(isToggledHiddenByDefault: true),
-                    Tables\Columns\TextColumn::make('status')
-                        ->badge()
-                        ->color(fn(string $state): string => match ($state) {
-                            'draft' => 'gray',
-                            'published' => 'success',
-                            'archived' => 'warning',
-                            default => 'gray',
-                        }),
-                    Tables\Columns\TextColumn::make('published_at')
-                        ->dateTime()
-                        ->sortable(),
-                    Tables\Columns\TextColumn::make('views')
-                        ->numeric()
-                        ->sortable(),
-                    Tables\Columns\TextColumn::make('created_at')
-                        ->dateTime()
-                        ->sortable()
-                        ->toggleable(isToggledHiddenByDefault: true),
-                ])
+                Tables\Columns\ImageColumn::make('featured_image'),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->limit(50),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->badge()
+                    ->color('info')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('author.name')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'draft' => 'gray',
+                        'published' => 'success',
+                        'archived' => 'warning',
+                        default => 'gray',
+                    }),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('views')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
             ->filters([
-                    Tables\Filters\SelectFilter::make('status')
-                        ->options([
-                                'draft' => 'Draft',
-                                'published' => 'Published',
-                                'archived' => 'Archived',
-                            ]),
-                    Tables\Filters\SelectFilter::make('category')
-                        ->relationship('category', 'name', fn(Builder $query) => $query->where('type', 'news')),
-                ])
-            ->actions([
-                    Tables\Actions\EditAction::make(),
-                    Tables\Actions\DeleteAction::make(),
-                ])
-            ->bulkActions([
-                    Tables\Actions\BulkActionGroup::make([
-                        Tables\Actions\DeleteBulkAction::make(),
+                Tables\Filters\SelectFilter::make('status')
+                    ->options([
+                        'draft' => 'Draft',
+                        'published' => 'Published',
+                        'archived' => 'Archived',
                     ]),
-                ]);
+                Tables\Filters\SelectFilter::make('category')
+                    ->relationship('category', 'name', fn(Builder $query) => $query->where('type', 'news')),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
     }
 
     public static function getRelations(): array
